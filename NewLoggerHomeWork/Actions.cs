2486eabdc41569088a5155135405134e3f89cc77
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-25 11:51:44 +0200

    update functionality in Actions

--- NewLoggerHomeWork/Actions.cs
+++ NewLoggerHomeWork/Actions.cs
@@ -10,16 +10,18 @@ namespace NewLoggerHomeWork
     {
         public void MethodFirst()
         {
+            Logger log = Logger.GetInstance();
+            log.Info(GlobalConstans.WarningLevels.Info, "Start method:"+nameof(MethodFirst));
             
-
         }
         public void MethodSecond()
         {
-            
+            Logger log = Logger.GetInstance();
+            log.Info(GlobalConstans.WarningLevels.Warning, "Skipped logic in method:"+nameof(MethodSecond));
         }
         public void MethodThird()
-        {
-            
+        {            
+            throw new Exception("I broke a toilet");
         }
     }
 }
