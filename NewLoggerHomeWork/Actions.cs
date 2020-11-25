04cdc6125aa85dbcffaa62f64d1fc2dd14f54549
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-25 11:50:25 +0200

    add actions class

---
+++ NewLoggerHomeWork/Actions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLoggerHomeWork
+{
+    class Actions
+    {
+        public void MethodFirst()
+        {
+            
+
+        }
+        public void MethodSecond()
+        {
+            
+        }
+        public void MethodThird()
+        {
+            
+        }
+    }
+}
