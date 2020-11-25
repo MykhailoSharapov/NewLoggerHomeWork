b1d6787834e583af71904a6b6fb440751adcf53e
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-25 11:29:33 +0200

    Create Logger Singleton

---
+++ NewLoggerHomeWork/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLoggerHomeWork
+{
+    class Logger
+    {
+        private Logger()
+        {
+
+        }
+        private static Logger instance;
+        private static List<string> LogMessages = new List<string>();
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+    }
+}
